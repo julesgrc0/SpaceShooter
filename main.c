61bad5eec5d3063a16e72a50569cc7e9b6bb94d5
JulesG10 <jules10avrg@gmail.com>
2021-07-05 16:29:50 +0200

    core game update

--- main.c
+++ main.c
@@ -1,39 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
 #include <pthread.h>
 #include <SDL2/SDL.h>
-#include "src/texture.h"
+#include "src/collision.h"
 
-
-typedef struct Player
+typedef enum MouseCode
 {
-    double x, y;
-} Player;
+    RIGHT,
+    LEFT,
+    WHELL_UP,
+    WHELL_DOWN,
+    MOVE,
+    NONE
+} MouseCode;
 
-struct GameData
+typedef struct UpdateInfo
 {
-    Player main_player;
-};
+    SDL_Keycode key;
+    Vector2 mouse_position;
+    MouseCode mouse;
+} UpdateInfo;
 
-static struct GameData *current_data;
-void update(void *data);
+void update(float, UpdateInfo);
+void draw(SDL_Renderer *);
 
 int main(int argc, char **argv)
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO))
+    {
+        return 1;
+    }
+
     SDL_Window *window;
     SDL_Renderer *renderer;
-    SDL_CreateWindowAndRenderer(500, 500, 0, &window, &renderer);
 
+    if (SDL_CreateWindowAndRenderer(500, 500, 0, &window, &renderer) != 0)
+    {
+        return 1;
+    }
+
+    /*
     pthread_t update_thread;
     pthread_create(&update_thread, NULL, (void *)update, (void *)current_data);
     pthread_detach(update_thread);
+    */
 
+    Uint32 current_time;
+    Uint32 last_time = SDL_GetTicks();
+    float deltatime = 0.0f;
+
+    time_t counter_start = time(0);
+    time_t counter_current = counter_start;
+    int frames = 0;
 
     bool running = true;
     while (running)
     {
+        counter_current = time(0);
+        frames++;
+        if ((counter_current - counter_start) >= 1)
+        {
+            printf("\rFPS: %d", frames);
+            fflush(stdout);
+            frames = 0;
+            counter_start = counter_current;
+        }
+        current_time = SDL_GetTicks();
+        deltatime = current_time - last_time;
+        deltatime /= 100;
+
+        UpdateInfo updateInfo;
+        updateInfo.mouse = NONE;
+        updateInfo.key = SDLK_UNKNOWN;
+
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
@@ -42,52 +83,62 @@ int main(int argc, char **argv)
             case SDL_QUIT:
                 running = false;
                 break;
+            case SDL_KEYDOWN:
+                updateInfo.key = event.key.keysym.sym;
+                break;
+            case SDL_MOUSEMOTION:
+                updateInfo.mouse = MOVE;
+
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                updateInfo.mouse = RIGHT;
+                break;
+            case SDL_MOUSEWHEEL:
+                if (event.button.y > 0)
+                {
+                    updateInfo.mouse = WHELL_UP;
+                }
+                else
+                {
+                    updateInfo.mouse = WHELL_DOWN;
+                }
+
+                break;
+            }
+            if (updateInfo.mouse != WHELL_UP && updateInfo.mouse != WHELL_DOWN)
+            {
+                updateInfo.mouse_position.x = event.button.x;
+                updateInfo.mouse_position.y = event.button.y;
             }
         }
 
-
+        update(deltatime, updateInfo);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
-
-
+        draw(renderer);
         SDL_RenderPresent(renderer);
+
+        /*CPU fix*/
+        SDL_Delay(1);
+        last_time = current_time;
     }
 
-    pthread_join(update_thread, NULL);
+    //pthread_join(update_thread, NULL);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
 
+float x = 0;
 
-void update(void *data)
+void draw(SDL_Renderer *renderer)
 {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_RenderDrawLine(renderer, x, 10, x + 10, 10);
+}
 
-    while (true)
-    {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            double delta = (double)(SDL_GetTicks() / 1000);
-
-            switch (event.type)
-            {
-            case SDL_KEYDOWN:
-
-                if (event.key.keysym.sym == SDLK_LEFT)
-                {
-                    current_data->main_player.x -= 5 * delta;
-                }
-                else if (event.key.keysym.sym == SDLK_RIGHT)
-                {
-                    current_data->main_player.x += 5 * delta;
-                }
-                printf("%f %f\n", delta, current_data->main_player.x);
-                break;
-            }
-        }
-
-    }
-    pthread_exit(NULL);
+void update(float deltatime, UpdateInfo info)
+{
+    x += deltatime * 2;
 }
